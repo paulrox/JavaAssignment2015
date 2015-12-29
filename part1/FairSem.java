90ae71bd6f4f7d5687f7fab01527e1df628b5c5b
Paolo Sassi <paolo.sassi91@gmail.com>
2015-12-29 17:28:24 +0100

    minor changes

--- part1/FairSem.java
+++ part1/FairSem.java
@@ -1,42 +1,42 @@
 package part1;
 
 public class FairSem {
-	private final int max_threads;
-	private long[] tid_queue;
-	private long owner_tid;
-	private int value;
-	private int front, rear, count;
+	private final int max_threads;		// max number of waiting threads
+	private long[] tid_queue;			// thread queue
+	private long extracted_tid;			// last extracted thread TID
+	private int value;					// semaphore value
+	private int front, rear, count;		// support for cyclic queue
 	
 	public FairSem(int n, int t_num) {
 		max_threads = t_num;
 		tid_queue = new long[max_threads];
 		value = n;
 		front = rear = count = 0;
-		owner_tid = 0;
+		extracted_tid = 0;
 	}
 	
 	public synchronized void fairWait() {
 		System.out.println(Thread.currentThread().getName() + " started P\n");
-		if (value == 0) {
+		if (value == 0) {	// red semaphore, add the current thread to the queue
 				count++;
 				tid_queue[rear] = Thread.currentThread().getId();
 				rear = (rear + 1) % max_threads;
-				while (owner_tid != Thread.currentThread().getId()) {
+				while (extracted_tid != Thread.currentThread().getId()) {
 					try { wait();
 					} catch (InterruptedException e) {}
 				}
+				extracted_tid = 0;	// once woke up, reset the extracted tid
 		} else value--;
 		System.out.println(Thread.currentThread().getName() + " terminated P\n");
 	}
 	
 	public synchronized void fairSignal() {
-		System.out.println(Thread.currentThread().getName() + " started V\n");
 		if (count > 0) {
 			count--;
-			owner_tid = tid_queue[front];
+			extracted_tid = tid_queue[front];
 			front = (front + 1) % max_threads;
 			notifyAll();
 		} else value++;
-		System.out.println(Thread.currentThread().getName() + " terminated V\n");
+		System.out.println(Thread.currentThread().getName() + " executed V\n");
 	}
 }
