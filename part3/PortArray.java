8e38b15d4f6370a52098279b6246031c7ad15456
Paolo Sassi <paolo.sassi91@gmail.com>
2016-01-05 22:44:25 +0100

    part 3 modified

--- part3/PortArray.java
+++ part3/PortArray.java
@@ -1,64 +1,66 @@
 package part3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 import part1.FairSem;
+import part2.SynchPort;
 
 public class PortArray<T> {
 	int port_num;
-	int last_index;
-	SynchPortElem<T> first;
+	List<SynchPort<T>> ports;
+	int[] waiting;
 	FairSem mutex, available;
 	
 	public PortArray(int n) {
-		SynchPortElem<T> tmp;
 		port_num = n;
-		last_index = 0;
-		tmp = new SynchPortElem<T>();
-		first = tmp;
-		for (int i = 0; i < n - 1; i++) {
-			tmp.next = new SynchPortElem<T>();
-			tmp = tmp.next;
-		}
-		tmp.next = null;
+		ports = new ArrayList<SynchPort<T>>();
 		mutex = new FairSem(1);
 		available = new FairSem(0);
+		waiting = new int[n];
+		for (int i = 0; i < n; i++) {
+			waiting[i] = 0;
+		}
 	}
 	
 	public void send(MessageInc<T> m, int n) {
-		SynchPortElem<T> tmp;
 		mutex.fairWait();
-		tmp = first;
-		for (int i = 0; i < n; i++) {
-			tmp = tmp.next;
-		}
-		tmp.msgAvailable = true;
+		waiting[n]++;
 		available.fairSignal();
 		mutex.fairSignal();
-		tmp.port.send(m.msg);
+		ports.get(n).send(m.msg);
 	}
 	
 	public MessageInc<T> receive(int[] v, int n) {
-		int index;
-		SynchPortElem<T> tmp;
-		MessageInc<T> m;
+		MessageInc<T> m = new MessageInc<T>();
+		int rand_i, j;
+		boolean found = false;
+		rand_i = ThreadLocalRandom.current().nextInt(0, port_num + 1);
+		j = 0;
 		available.fairWait();
 		mutex.fairWait();
-		tmp = first;
-		index = last_index;
-		for (int i = 0; i < last_index; i++) {
-			tmp = tmp.next;
-			index = (index + 1) % port_num; 
-		}
-		while (tmp.msgAvailable == false) {
-			if (tmp.next != null) {
-				tmp = tmp.next;
-				index = (index + 1) % port_num; 
-			} else {
-				tmp = first;
-				index = 0;
+		while (!found) {
+			for (int i = 0; i < n; i++) {
+				j = rand_i;
+				do {
+					if (j == v[i] && waiting[j] > 0) {
+						found = true;
+					} else {
+						j = (j + 1) % port_num;
+					}
+				} while (j > rand_i || found != true);
+			}
+			if (!found) {
+				mutex.fairSignal();
+				available.fairWait();
+				mutex.fairWait();
 			}
 		}
-		last_index = index;
-		m = new MessageInc
-		
+		waiting[j]--;
+		mutex.fairSignal();
+		m.msg = ports.get(j).receive();
+		m.p_index = j;
+		return m;	
 	}
 }
